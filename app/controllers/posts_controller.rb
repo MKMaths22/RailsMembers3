7eb54543d3a95817117762c598bfdbe844d8e7fe
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-12-09 09:59:12 +0000

    Write methods in Posts controller, giving the created post the user_id of the current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,35 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user, except: :index
+  
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(allowed_post_params)
+    @post[:user_id] = @user[:id]
+
+    if @post.save
+      redirect_to posts_path
+    else
+      puts "Errors are #{@post.errors.full_messages}"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def authenticate_user
+    @user = current_user
+    # access_denied view not defined
+    render 'access_denied', status: :access_denied if @user.nil?
+  end
+  
+  def allowed_post_params
+    params.require(:post).permit(:title, :body)
   end
 end
