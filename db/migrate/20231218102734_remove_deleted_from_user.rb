2f408476915cc965097e719f681289472d5053b4
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-12-18 10:39:40 +0000

    Use migration to remove redundant deleted property from User model

---
+++ db/migrate/20231218102734_remove_deleted_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveDeletedFromUser < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :users, :deleted
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_18_094455) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_18_102734) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_18_094455) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
